78bb6b720d58001ea570a4319783ce129664f499
yokoyang <yokocc255@163.com>
2019-03-27 21:10:12 +0800

    2019 网易

--- src/main/java/WY_4.java
+++ src/main/java/WY_4.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
 
 public class WY_4 {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         String process = scanner.next();
-        char[] dir = {'N', 'E', 'S', 'W'};
+        String dir = "NESW";
         int ans = 0;
         for (int i = 0; i != n; i++) {
             ans = (ans + (process.charAt(i) == 'L' ? -1 : 1) + 4) % 4;
         }
-        System.out.println(dir[ans]);
+        System.out.println(dir.charAt(ans));
 
     }
 }
--- src/main/java/WY_5.java
+++ src/main/java/WY_5.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
 
 public class WY_5 {
+    // 时间复杂度O(N)
+// 将除数y从k+1 开始计算，除数为y时，数对的个数包括两部分： n/y*(y-k) 和多出来的另一部分，这部分需要看
+// n%y 和k的大小关系
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        String process = scanner.next();
-        char[] dir = {'N', 'E', 'S', 'W'};
-        int ans = 0;
-        for (int i = 0; i != n; i++) {
-            ans = (ans + (process.charAt(i) == 'L' ? -1 : 1) + 4) % 4;
+    public static void main(String[] arsg){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        long sum = 0;
+        int t = 0;
+        int tt = 0;
+        for(int i=k+1;i<=n;i++){
+            t++;
+            tt = (n%i - k + 1) >0 ? (n%i - k + 1):0;
+            sum+=n/i*t+tt;
         }
-        System.out.println(dir[ans]);
-
+        if(k == 0) sum-=n;// k=0 特殊情况  多计算了n次
+        System.out.print(sum);
     }
 }
