4687c5e4683ad1db656985dc924b4e279557c0de
yokoyang <yokocc255@163.com>
2019-05-13 22:00:03 +0800

    knapsack

---
+++ src/main/java/Algorithm/BackTrace/Backtracking.java
@@ -0,0 +1,90 @@
+package Algorithm.BackTrace;
+
+public class Backtracking {
+    int[] result = new int[8];// 全局或成员变量, 下标表示行, 值表示 queen 存储在哪一列
+
+    public void cal8queens(int row) { // 调用方式：cal8queens(0);
+        if (row == 8) { // 8 个棋子都放置好了，打印结果
+            printQueens(result);
+            return; // 8 行棋子都放好了，已经没法再往下递归了，所以就 return
+        }
+        for (int column = 0; column < 8; ++column) { // 每一行都有 8 中放法
+            if (isOk(row, column)) { // 有些放法不满足要求
+                result[row] = column; // 第 row 行的棋子放到了 column 列
+                cal8queens(row + 1); // 考察下一行
+            }
+        }
+    }
+
+    private boolean isOk(int row, int column) {// 判断 row 行 column 列放置是否合适
+        int leftup = column - 1, rightup = column + 1;
+        for (int i = row - 1; i >= 0; --i) { // 逐行往上考察每一行
+            if (result[i] == column) return false; // 第 i 行的 column 列有棋子吗？
+            if (leftup >= 0) { // 考察左上对角线：第 i 行 leftup 列有棋子吗？
+                if (result[i] == leftup) return false;
+            }
+            if (rightup < 8) { // 考察右上对角线：第 i 行 rightup 列有棋子吗？
+                if (result[i] == rightup) return false;
+            }
+            --leftup;
+            ++rightup;
+        }
+        return true;
+    }
+
+    private void printQueens(int[] result) { // 打印出一个二维矩阵
+        for (int row = 0; row < 8; ++row) {
+            for (int column = 0; column < 8; ++column) {
+                if (result[row] == column) System.out.print("Q ");
+                else System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private int maxW = Integer.MIN_VALUE; // 结果放到 maxW 中
+    private int[] weight = {2, 2, 4, 6, 3};  // 物品重量
+    private int n = 5; // 物品个数
+    private int w = 9; // 背包承受的最大重量
+    private boolean[][] mem = new boolean[5][10]; // 备忘录，默认值 false
+
+    public void f(int i, int cw) { // 调用 f(0, 0)
+        if (cw == w || i == n) { // cw==w 表示装满了，i==n 表示物品都考察完了
+            if (cw > maxW) maxW = cw;
+            return;
+        }
+        if (mem[i][cw]) return; // 重复状态
+        mem[i][cw] = true; // 记录 (i, cw) 这个状态
+        f(i + 1, cw); // 选择不装第 i 个物品
+        if (cw + weight[i] <= w) {
+            f(i + 1, cw + weight[i]); // 选择装第 i 个物品
+        }
+    }
+
+    //    weight: 物品重量，n: 物品个数，w: 背包可承载重量
+    public int knapsack(int[] weight, int n, int w) {
+        boolean[][] states = new boolean[n][w + 1]; // 默认值 false
+        states[0][0] = true;  // 第一行的数据要特殊处理，可以利用哨兵优化
+        states[0][weight[0]] = true;
+        for (int i = 1; i < n; ++i) { // 动态规划状态转移
+            for (int j = 0; j <= w; ++j) {// 不把第 i 个物品放入背包
+                if (states[i - 1][j]) states[i][j] = states[i - 1][j];
+            }
+            for (int j = 0; j <= w - weight[i]; ++j) {// 把第 i 个物品放入背包
+                if (states[i - 1][j]) states[i][j + weight[i]] = true;
+            }
+        }
+        for (int i = w; i >= 0; --i) { // 输出结果
+            if (states[n - 1][i]) return i;
+        }
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        Backtracking backtracking = new Backtracking();
+//        backtracking.cal8queens(0);
+        backtracking.f(0, 0);
+    }
+}
