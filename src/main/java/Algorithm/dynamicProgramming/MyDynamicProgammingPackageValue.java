1661853d36cc17530103de30fa443163a2c6f3dc
yokoyang <yokocc255@163.com>
2019-05-14 20:04:24 +0800

    canPartition

--- src/main/java/Algorithm/dynamicProgramming/MyDynamicProgammingPackageValue.java
+++ src/main/java/Algorithm/dynamicProgramming/MyDynamicProgammingPackageValue.java
@@ -1,5 +1,7 @@
 package Algorithm.dynamicProgramming;
 
+import java.util.Arrays;
+
 /**
  * 使用动态规则来解决0-1背包求解最大价值的问题
  *
@@ -11,9 +13,9 @@ public class MyDynamicProgammingPackageValue {
      * 使用动态规则来求物品的最大价值
      *
      * @param weights 物品信息
-     * @param values 最大价值
-     * @param maxNum 最大数量
-     * @param weight 最大重量
+     * @param values  最大价值
+     * @param maxNum  最大数量
+     * @param weight  最大重量
      * @return 求最大价值
      */
     public int dynamicProgammingPackageMaxValue(int[] weights, int[] values, int maxNum, int weight) {
@@ -68,9 +70,9 @@ public class MyDynamicProgammingPackageValue {
      * 使用动态规则来求物品的最大价值
      *
      * @param weights 物品信息
-     * @param values 最大价值
-     * @param maxNum 最大数量
-     * @param weight 最大重量
+     * @param values  最大价值
+     * @param maxNum  最大数量
+     * @param weight  最大重量
      * @return 求最大价值
      */
     public int dynamicProgammingPackageMaxValueMem(
@@ -112,4 +114,147 @@ public class MyDynamicProgammingPackageValue {
 
         return resultValue;
     }
+
+    // items 商品价格，n 商品个数, w 表示满减条件，比如 200
+    public static void double11advance(int[] items, int n, int w) {
+        boolean[][] states = new boolean[n][3 * w + 1];// 超过 3 倍就没有薅羊毛的价值了
+        states[0][0] = true;  // 第一行的数据要特殊处理
+        states[0][items[0]] = true;
+        for (int i = 1; i < n; ++i) { // 动态规划
+            for (int j = 0; j <= 3 * w; ++j) {// 不购买第 i 个商品
+                if (states[i - 1][j]) states[i][j] = states[i - 1][j];
+            }
+            for (int j = 0; j <= 3 * w - items[i]; ++j) {// 购买第 i 个商品
+                if (states[i - 1][j]) states[i][j + items[i]] = true;
+            }
+        }
+
+        int j;
+        for (j = w; j < 3 * w + 1; ++j) {
+            if (states[n - 1][j]) break; // 输出结果大于等于 w 的最小值
+        }
+        if (j == 3 * w + 1) return; // 没有可行解
+        for (int i = n - 1; i >= 1; --i) { // i 表示二维数组中的行，j 表示列
+            if (j - items[i] >= 0 && states[i - 1][j - items[i]]) {
+                System.out.print(items[i] + " "); // 购买这个商品
+                j = j - items[i];
+            } // else 没有购买这个商品，j 不变。
+        }
+        if (j != 0) System.out.print(items[0]);
+    }
+
+    private int[][] matrix = {{1, 3, 5, 9}, {2, 1, 3, 4}, {5, 2, 6, 7}, {6, 8, 4, 3}};
+    private int n = 4;
+    private int[][] mem = new int[4][4];
+
+    public int minDist(int i, int j) { // 调用 minDist(n-1, n-1);
+        if (i == 0 && j == 0) return matrix[0][0];
+        if (mem[i][j] > 0) return mem[i][j];
+        int minLeft = Integer.MAX_VALUE;
+        if (j - 1 >= 0) {
+            minLeft = minDist(i, j - 1);
+        }
+        int minUp = Integer.MAX_VALUE;
+        if (i - 1 >= 0) {
+            minUp = minDist(i - 1, j);
+        }
+
+        int currMinDist = matrix[i][j] + Math.min(minLeft, minUp);
+        mem[i][j] = currMinDist;
+        return currMinDist;
+    }
+
+    public static void main(String[] args) {
+        Solution myDynamicProgammingPackageValue = new Solution();
+//        int[] coins = new int[]{186, 419, 83, 408};
+        int[] coins = new int[]{1, 5, 5, 11};
+        System.out.println(myDynamicProgammingPackageValue.canPartition(coins));
+//        int minDist = myDynamicProgammingPackageValue.coinChange(coins, 6249);
+//        System.out.println(minDist);
+    }
+
+    static class Solution {
+        public boolean canPartition2(int[] nums) {
+            int total = 0;
+            for (int i : nums) total += i; // compute the total sum of the input array
+            if (total % 2 != 0)
+                return false; // if the array sum is not even, we cannot partition it into 2 equal subsets
+            int max = total / 2; // the maximum for a subset is total/2
+            int[][] results = new int[nums.length][max]; // integer matrix to store the results, so we don't have to compute it more than one time
+            return isPartitionable(max, 0, 0, nums, results);
+        }
+
+        public boolean isPartitionable(int max, int curr, int index, int[] nums, int[][] results) {
+            if (curr > max || index > nums.length - 1) {
+                return false; // if we passed the max, or we reached the end of the array, return false
+            }
+            if (curr == max) return true; // if we reached the goal (total/2) we found a possible partition
+            if (results[index][curr] == 1) {
+                return true; // if we already computed teh result for the index i with the sum current, we retrieve this result (1 for true)
+            }
+            if (results[index][curr] == 2) {
+                return false; // if we already computed teh result for the index i with the sum current, we retrieve this result (2 for false)
+            }
+            boolean res = isPartitionable(max, curr + nums[index], index + 1, nums, results) || isPartitionable(max, curr, index + 1, nums, results); // else try to find the equal partiion, taking this element, or not taking it
+            results[index][curr] = res ? 1 : 2; // store the result for this index and this current sum, to use it in dynamic programming
+            return res;
+        }
+
+        public boolean canPartition(int[] nums) {
+            int sum = 0;
+            for (int n : nums) {
+                sum += n;
+            }
+            //奇数
+            if (sum % 2 != 0) {
+                return false;
+            }
+            int half = sum >> 1;
+            boolean[][] dp = new boolean[nums.length + 1][half + 1];
+            for (int i = 0; i <= nums.length; i++) {
+                dp[i][0] = true;
+            }
+
+            for (int i = 1; i < nums.length + 1; i++) {
+                for (int j = 0; j < half + 1; j++) {
+                    if (dp[i - 1][j]) {
+                        dp[i][j] = true;
+                        continue;
+                    }
+                    if (j - nums[i - 1] >= 0 && dp[i - 1][j - nums[i - 1]]) {
+                        dp[i][j] = true;
+                    }
+                }
+            }
+            return dp[nums.length][half];
+        }
+
+        public int coinChange(int[] coins, int amount) {
+            if (amount < 0) {
+                return -1;
+            }
+            if (amount == 0) {
+                return 0;
+            }
+            Arrays.sort(coins);
+            int[] dp = new int[amount + 1];
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            dp[0] = 0;
+
+            for (int i = 1; i <= amount; i++) {
+                for (int coin : coins) {
+                    if (coin > i) {
+                        break;
+                    }
+                    if (dp[i - coin] != Integer.MAX_VALUE) {
+                        dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+                    }
+                }
+            }
+
+            return dp[amount] != Integer.MAX_VALUE ? dp[amount] : -1;
+        }
+    }
+
+
 }
