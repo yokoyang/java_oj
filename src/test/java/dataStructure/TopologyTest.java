9f27663d581561c912ebaf9f78829eda79a39e0e
yokoyang <yokocc255@163.com>
2019-05-17 14:35:22 +0800

    networkDelayTime

--- src/test/java/dataStructure/TopologyTest.java
+++ src/test/java/dataStructure/TopologyTest.java
@@ -33,6 +33,20 @@ public class TopologyTest {
         instance.topoSortByDFS();
         // instance.topoSortByKahn();
     }
+    @Test
+    public void t2(){
+        int [][] a = new int[2][3];
+        a[0][0] = 1;
+        a[1][1] = 2;
+        a[1][0] = -2;
+        a[1][2] = 4;
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println("\n");
+        }
+    }
 //    @Test
 //    public void testKnhn2() {
 //        Graph instance = new Graph(10);
