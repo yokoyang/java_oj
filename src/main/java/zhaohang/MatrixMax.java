ce151a2b49eec624a3739db89278c68e64a58ed3
yokoyang <yokocc255@163.com>
2019-09-01 19:45:37 +0800

    findCheapestPrice

--- src/main/java/zhaohang/MatrixMax.java
+++ src/main/java/zhaohang/MatrixMax.java
@@ -108,4 +108,19 @@ public class MatrixMax {
         return false;
 
     }
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i + 1;
+                result[0] = map.get(target - nums[i]);
+                return result;
+            }
+            map.put(nums[i], i + 1);
+        }
+        return result;
+    }
 }
