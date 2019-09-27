50770b49f45a35d99e6f9bcc7bb0d07e3c2ce908
yokoyang <yokocc255@163.com>
2019-09-27 17:03:46 +0800

    SegmentTree

--- src/main/java/Algorithm/lineSegmenttree/SegmentTree.java
+++ src/main/java/Algorithm/lineSegmenttree/SegmentTree.java
@@ -121,22 +121,38 @@ public class SegmentTree {
 
     // Driver program to test above functions
     public static void main(String args[]) {
-        int arr[] = {1, 3, 5, 7, 9, 11};
+        int arr[] = {9, -8};
         int n = arr.length;
         SegmentTree tree = new SegmentTree(arr, n);
 
-        // Build segment tree from given array
-
-        // Print sum of values in array from index 1 to 3
-        System.out.println("Sum of values in given range = " +
-                tree.getSum(n, 1, 3));
-
         // Update: set arr[1] = 10 and update corresponding segment
         // tree nodes
-        tree.updateValue(arr, n, 1, 10);
+        tree.updateValue(arr, n, 0, 3);
 
         // Find sum after the value is updated
         System.out.println("Updated sum of values in given range = " +
-                tree.getSum(n, 1, 3));
+                tree.getSum(n, 1, 1));
+        System.out.println("Updated sum of values in given range = " +
+                tree.getSum(n, 0, 1));
+        tree.updateValue(arr, n, 1, -3);
+        System.out.println("Updated sum of values in given range = " +
+                tree.getSum(n, 0, 1));
+//        int arr[] = {1, 3, 5, 7, 9, 11};
+//        int n = arr.length;
+//        SegmentTree tree = new SegmentTree(arr, n);
+//
+//        // Build segment tree from given array
+//
+//        // Print sum of values in array from index 1 to 3
+//        System.out.println("Sum of values in given range = " +
+//                tree.getSum(n, 1, 3));
+//
+//        // Update: set arr[1] = 10 and update corresponding segment
+//        // tree nodes
+//        tree.updateValue(arr, n, 1, 10);
+//
+//        // Find sum after the value is updated
+//        System.out.println("Updated sum of values in given range = " +
+//                tree.getSum(n, 1, 3));
     }
 }
