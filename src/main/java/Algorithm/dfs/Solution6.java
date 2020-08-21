a80d21a7b346a1badab92a99ffb7f33e97010fc6
奇方 <yokocc255@163.com>
2020-08-21 14:17:08 +0800

    [add] greedy

--- src/main/java/Algorithm/dfs/Solution6.java
+++ src/main/java/Algorithm/dfs/Solution6.java
@@ -14,7 +14,6 @@ public class Solution6 {
     }
 
     public static void main(String[] args) {
-
         Solution6 solution = new Solution6();
         solution.fourSum(new int[]{1, 0, -1, 0, -2, 2},
                 0);
--- src/main/java/designPattern/singleton/EnumAttackTest.java
+++ src/main/java/designPattern/singleton/EnumAttackTest.java
@@ -8,10 +8,14 @@ import java.lang.reflect.InvocationTargetException;
 public class EnumAttackTest {
     public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
         EnumSingleton enumSingleton = EnumSingleton.getEnumSingleton();
-        //序列化攻击
-        byte[] serialize4 = SerializationUtils.serialize(enumSingleton);
-        EnumSingleton enumSingleton2 = (EnumSingleton) SerializationUtils.deserialize(serialize4);
-        System.out.println(enumSingleton == enumSingleton2);
+        EnumSingleton enumSingleton2 = EnumSingleton.getEnumSingleton();
+
+        enumSingleton.getValue();
+        enumSingleton2.getValue();
+//        //序列化攻击
+//        byte[] serialize4 = SerializationUtils.serialize(enumSingleton);
+//        EnumSingleton enumSingleton2 = (EnumSingleton) SerializationUtils.deserialize(serialize4);
+//        System.out.println(enumSingleton == enumSingleton2);
 //
 //        //反射攻击
 //        Constructor<EnumSingleton> enumSingletonConstructor = EnumSingleton.class.getDeclaredConstructor();
